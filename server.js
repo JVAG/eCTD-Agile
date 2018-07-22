78dd7f3650458f28dbd995f0757412d915da0b27
Snehal Indurkar <sneha774@gmail.com>
2018-07-22 06:44:46 -0500

    Changed connection string

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use('/lib', express.static('bower_components'));
 
-Mongoose.createConnection('mongodb://root:uymhDsiWFZQ7@localhost:27017/ectd2?authSource=admin', function(err) {
+Mongoose.connect('mongodb://localhost:27017/ectd2', function(err) {
     if(err){
         console.log('Not connected to database ', err);
     }
